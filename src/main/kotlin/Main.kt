65f35f094fff2035bee8f43569b72628c38e8637
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-01-11 19:08:44 +0100

    Solved day 4 task 1ˆ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import okio.FileSystem
+import okio.Path.Companion.toPath
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+var fullyContainedRanges = 0
+
+fun main(args: Array<String>) {
+    val path = args[0].toPath()
+
+    FileSystem.SYSTEM.read(path) {
+        while (true) {
+            val line = readUtf8Line() ?: break
+            if (line.isEmpty()) break
+
+            val ranges = parseLine(line)
+            val intersection = ranges.first.intersect(ranges.second)
+
+            if (intersection.isEmpty()) continue
+            if (intersection == ranges.first.toSet() || intersection == ranges.second.toSet()) {
+                ++fullyContainedRanges
+            }
+        }
+        println(fullyContainedRanges)
+    }
+}
+
+fun parseLine(line: String): Pair<IntRange, IntRange> {
+    val rangeStrings = line.split(",")
+    return Pair(parseRange(rangeStrings[0]), parseRange(rangeStrings[1]))
+}
+
+fun parseRange(rangeString: String): IntRange {
+    val rangeLimits = rangeString.split("-")
+    val rangeStart = rangeLimits[0].toInt()
+    val rangeEnd = rangeLimits[1].toInt()
+    return rangeStart..rangeEnd
 }
